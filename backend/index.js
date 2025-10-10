3481b43ff683ccb3a48e8757b987cc8ea9ca0986
Justdoit-ahn <ansaebi@gmail.com>
2025-10-10 11:29:55 +0900

    2025.10.10-0

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,25 @@
+require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
 const app = express();
 const PORT = "3000";
+
+const userRoutes = require("./routes/user");
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded());
+
+app.use("/api/auth", userRoutes);
 
 app.get("/", (req, res) => {
   res.send("hello world");
 });
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB와 연결이 되었습니다."))
+  .catch((error) => console.log("연결이 실패함.!!!!!", error));
 
 app.listen(PORT, () => {
   console.log("서버가 동작중입니다.");
